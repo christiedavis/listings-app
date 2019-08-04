dd97f286ba7c708f51e7a3ad114fc70c7be8739d
Christie Davis <christie@alphero.com>
2019-08-04 21:49:09 +1200

    Add job dto

--- ListingsApp/ListingsApp/Network Service/NetworkService.swift
+++ ListingsApp/ListingsApp/Network Service/NetworkService.swift
@@ -9,22 +9,17 @@
 import Foundation
 import PromiseKit
 
-protocol AccountNetworkServiceProtocol: class {
-    func getUserAccounts() -> Promise<BankAccountCollectionDTO>
-    func getUserTransactionFeed() -> Promise<UserTransactionFeedDTO>
+protocol JobNetworkServiceProtocol: class {
+    func getJobs() -> Promise<[JobDTO]>
 }
 
-class AccountNetworkService: BaseNetworkService {
+class JobNetworkService: BaseNetworkService {
     private let accountsPath = "/accounts"
     private var transactionFeedPath = "/feed/account/012b2ab8-d0f4-4aa3-ab8e-028fabbd228f/category/75ffc012-5854-4735-8ba5-810a2992b104"
 }
 
-extension AccountNetworkService: AccountNetworkServiceProtocol {
-    func getUserAccounts() -> Promise<BankAccountCollectionDTO> {
-        return self.request(method: .get, path: self.accountsPath)
-    }
-    
-    func getUserTransactionFeed() -> Promise<UserTransactionFeedDTO> {
-        return self.request(method: .get, path: self.transactionFeedPath)
+extension JobNetworkService: JobNetworkServiceProtocol {
+    func getJobs() -> Promise<[JobDTO]> {
+        
     }
 }
---
+++ ListingsApp/ListingsApp/NetworkModels/JobDTO.swift
@@ -0,0 +1,13 @@
+//
+//  JobDTO.swift
+//  ListingsApp
+//
+//  Created by Christie Davis on 4/08/19.
+//  Copyright © 2019 Christie-Davis. All rights reserved.
+//
+
+import UIKit
+
+class JobDTO: Codable {
+
+}
